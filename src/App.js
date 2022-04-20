946068f19a6dc6e58050b7156d76469d3d40897e
sankettchauhan <sanket.chauhan4@gmail.com>
2022-04-20 23:18:38 +0530

    add react router dom , home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+// import Article from "./pages/article";
+// import ArticleNew from "./pages/articlenew";
+// import ArticleSearch from "./pages/articlesearch";
+import Home from "./pages/home";
+import "./index.css";
 
 function App() {
-  return <div className="App text-3xl">test</div>;
+  return (
+    <Router>
+      <Routes>
+        {/* <Route exact path="/article/:id" element={<Article />} />
+        <Route exact path="/article/add" element={<ArticleNew />} />
+        <Route exact path="/article/search" element={<ArticleSearch />} /> */}
+        <Route exact path="/home" element={<Home />} />
+        <Route path="/*" element={<Navigate to="/home" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
